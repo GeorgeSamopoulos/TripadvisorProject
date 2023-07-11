9eda055b53f220d7d7ede20d4789b7f73d40b402
GeorgeSamopoulos <134342388+GeorgeSamopoulos@users.noreply.github.com>
2023-07-11 16:36:24 +0300

    Delete tripadvisorreviewpage directory

--- tripadvisorreviewpage/app/public/wp-content/plugins/review-widgets-for-tripadvisor/uninstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once plugin_dir_path( __FILE__ ) . 'trustindex-plugin.class.php';
-$trustindex_pm_tripadvisor = new TrustindexPlugin_tripadvisor("tripadvisor", __FILE__, "10.3", "WP Tripadvisor Review Widgets", "Tripadvisor");
-$trustindex_pm_tripadvisor->uninstall();
-?>
